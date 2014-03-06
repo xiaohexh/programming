0c2ca97cd478082283500a0207d9ee08abd7ef39
xiaohe <xiaohexh@hotmail.com>
2014-03-05 19:07:06 -0800

    add makefile exercise

---
+++ cpp/make_proj/CommonDef.h
@@ -0,0 +1,6 @@
+#ifndef _COMMON_DEF_H__
+#define _COMMON_DEF_H__
+
+#define MAXFILESIZE		10
+
+#endif // _COMMON_DEF_H__
---
+++ cpp/make_proj/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "CommonDef.h"
+
+int main(int argc, char **argv)
+{
+	int a = MAXFILESIZE;
+	printf("max file size: %d\n", a);
+	printf("Hello\n");
+	return 0;
+}
